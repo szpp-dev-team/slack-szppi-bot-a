78c6602ec60ecd0ddefa4cf0b4f1c18b38420a13
mistralll <miyakei20011007@gmail.com>
2022-12-02 20:27:54 +0900

    feat: scrape.go and test

---
+++ scrape/scrape.go
@@ -0,0 +1,11 @@
+package scrape
+
+import (
+	"github.com/szpp-dev-team/szpp-slack-bot/model"
+)
+
+func Scrape() (model.ScrapeResult, error) {
+	var results model.ScrapeResult
+
+	return results, nil
+}
